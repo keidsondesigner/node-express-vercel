36fb90fd6b04fb2bb470df9f99c9a9be25d0526d
keidsondesigner <keidsondesigner@gmail.com>
2022-11-24 23:00:28 -0400

    post

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   return res.status(200).json({
     title: "Express Testing",
     message: "The app is working properly!",
