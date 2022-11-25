280654836a26594b52552b970daee188f47780c1
keidsondesigner <keidsondesigner@gmail.com>
2022-11-24 23:12:52 -0400

    test get

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const test = [];
+
 router.post("/", async (req, res, next) => {
+  test.push(req.body);
   return res.status(200).json(req.body);
+});
+
+
+router.get("/", async (req, res, next) => {
+  test.push(req.body);
+  return res.status(200).json(test);
 });
 
 module.exports = router;
